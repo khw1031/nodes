8136a88c0274acb0b29a9deed90646cb3baa16eb
khw1031 <khw1031@gmail.com>
2021-10-07 17:53:22 +0900

    Init

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,69 @@
-import path from "path";
-import express from "express";
+// Generic function
+// type constraints
 
-const app = express();
-const port = 3003;
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
 
-app.get("/", (req, res) => {});
+const mergedObj = merge({ name: "Max" }, { age: 20 }); // OK
 
-app.listen(port, () => {
-  console.log("Server is up on http://localhost:" + port);
-});
+// console.log(mergedObj);
+
+interface Lenghty {
+  length: number;
+}
+
+function countAndDescribe<T extends Lenghty>(element: T) {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+function extractAndConvert<T, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+}
+
+class DataStorage<T> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Kim");
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function crateCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
