b467d4331b99c075a31dfb56cb14a4c32dd47a79
khw1031 <khw1031@gmail.com>
2021-10-10 15:20:31 +0900

    practice

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 import "../app.css";
 
+declare var GLOBAL: string;
+
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
